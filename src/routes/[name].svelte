0876019391c9db4d221eb9298d161f84f703daa3
blck-snwmn <whitesnowmancreator@gmail.com>
2022-08-13 10:41:07 +0900

    Add named page sample

---
+++ src/routes/[name].svelte
@@ -0,0 +1,15 @@
+<script context="module">
+	let name = '';
+	/** @type {import('./__types/[name]').Load} */
+	export async function load({ params }) {
+		name = params.name;
+	}
+</script>
+
+<svelte:head>
+	<title>Greet</title>
+</svelte:head>
+
+<h1>Hello {name}</h1>
+
+<a href="/">Home</a>
